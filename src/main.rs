da98d81e5e146082e2b3a6a437d82f48b940d931
jtrefil <jirka.trefilu@gmail.com>
2024-12-28 15:36:03 +0100

    message

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 #![allow(unused_imports)]
-use std::net::TcpListener;
+use std::{io::Write, net::TcpListener};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:9092").unwrap();
      for stream in listener.incoming() {
          match stream {
-             Ok(_stream) => {
-                 println!("accepted new connection");
+             Ok(mut _stream) => {
+               let bytes_sent= _stream.write(&[0,0,7]).unwrap();  
              }
              Err(e) => {
                  println!("error: {}", e);
