76a5e45ff541e8ee872bcf4aa1cdfa18dc38c8e6
jtrefil <jirka.trefilu@gmail.com>
2024-12-30 19:14:59 +0100

    test

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,19 @@
 #![allow(unused_imports)]
 use std::{io::{Read, Write}, net::TcpListener};
-
-
-
-fn parse_correlation_id_v2(buffer:&[u8]) -> i32{
-    println!("{:?}",buffer);
-   let byte_offset = 8;
-   let correlation_id = i32::from_be_bytes(buffer[byte_offset..byte_offset+4].try_into().unwrap());
-   println!("Extracted correlation id: {correlation_id}");
-   return correlation_id; 
-}
+pub mod util;
+use util::kafka_header_util;
+use util::byte_util::stream_input_to_bytes;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:9092").unwrap();
      for stream in listener.incoming() {
          match stream {
-             Ok(mut _stream) => {
-                let mut buf: [u8;1024] = [0;1024];
-                let msg = _stream.read(&mut buf).unwrap();
-                println!("recieved {msg} bytes");
-                let correlation_id:i32 = parse_correlation_id_v2(&buf);
-                let message_size:i32 = 4;
-                let response = [message_size.to_be_bytes(), correlation_id.to_be_bytes()].concat(); 
-                let bytes_send = _stream.write(&response).expect("Failed to send");
-                println!("Sending response: {:?}", response);
+             Ok(mut stream) => {
+                let buf = match stream_input_to_bytes(&mut stream){
+                    Some(buffer) => buffer,
+                    None => panic!("No input!")
+                };
+                println!("extracted buffer: {:?}", buf);
              }
              Err(e) => {
                  println!("error: {}", e);
