e26ab09091c48811da222ebf97ec9da2dc77df7a
jtrefil <jirka.trefilu@gmail.com>
2024-12-30 20:27:32 +0100

    test

--- src/util/kafka_response_util.rs
+++ src/util/kafka_response_util.rs
@@ -1,8 +1,9 @@
 use std::{io::Write, net::TcpStream};
 
-pub fn send_response(stream:&mut TcpStream, buf: &Vec<u8>){
-    println!("Sending response: {:?}", buf);
-    let result = stream.write(buf);
+pub fn send_response(stream:&mut TcpStream, buf:&mut Vec<u8>){
+    let mut response_body: Vec<u8> = buf.len().to_be_bytes().to_vec();
+    response_body.append(buf);
+    let result = stream.write(&response_body);
     match result{
         Ok(bytes_send) => {
             println!("{bytes_send} bytes send");
