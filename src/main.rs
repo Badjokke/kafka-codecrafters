c8a195fbca209781a1bcd23467625da31d61da91
jtrefil <jirka.trefilu@gmail.com>
2025-01-01 15:40:13 +0100

    test

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,7 @@ fn main() {
                 if api_key == kafka_constants::KAFKA_API_VERSIONS_KEY{
                     let api_versions_body = 
                     kafka_response_util::create_api_version_response(kafka_constants::NO_ERROR,
-                         1,
+                         0,
                          3, 4, 0);
                          items.push(Box::new(api_versions_body));
                     send_response(&mut stream, &create_response(items));
