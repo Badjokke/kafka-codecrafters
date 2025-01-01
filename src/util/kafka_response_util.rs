064be866a814ddb1188f21651edaa2da55960eaf
jtrefil <jirka.trefilu@gmail.com>
2025-01-01 18:19:21 +0100

    test

--- src/util/kafka_response_util.rs
+++ src/util/kafka_response_util.rs
@@ -14,7 +14,7 @@ impl ToBytes for ApiVersionsResponse{
         //error_code
         bytes.extend(&self.error_code.to_be_bytes());
         //api version count
-        bytes.extend(&(1 as i32).to_be_bytes());
+        bytes.extend(&(1 as i16).to_be_bytes());
         //api version "struct"
         bytes.extend(&self.api_key.to_be_bytes());
         bytes.extend(&self.min_version.to_be_bytes());
