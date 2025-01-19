ffda9177e973dd04bfc1894c146f01bd93f55a0e
jtrefil <jirka.trefilu@gmail.com>
2025-01-19 23:48:10 +0100

    api_versions_support

--- src/client/tcp_client.rs
+++ src/client/tcp_client.rs
@@ -47,10 +47,6 @@ fn handle_client_message(buf: Vec<u8>) -> Option<Vec<u8>>{
     println!("Handling client message: {:?}", buf); 
     let (api_key, api_version, correlation_id, client_id, header_offset)= kafka_header_util::parse_header(&buf);
     let error_code = get_kafka_error_code(api_key, api_version);
-    if error_code == kafka_constants::UNSUPPORTED_API_VERSION_ERROR_CODE{
-        println!("Unknown API version: {api_key}");
-        return None;
-    }
     let res = create_kafka_response(buf[header_offset..].to_vec(), api_key, error_code).expect("!!!");
     let mut items: Vec<Box<dyn ToBytes>> = Vec::new();
     items.push(Box::new(correlation_id));
